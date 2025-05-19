6cc50a158256428cf6533bf6e3b962245d3e3a3a
kishlay <kishlay141@gmail.com>
2025-05-19 13:34:15 +0530

    removed the useless tags

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -35,7 +35,7 @@ const Certificates = () => {
       title: "Introduction To Internet Of Things ",
       description:
         "Completed an in-depth course on networking concepts including protocols, OSI layers, and real-world applications.",
-      tags: ["Udemy", "Online Learning", "Skill Development"],
+      tags: [],
       link: "https://drive.google.com/file/d/1rnXc6F6eRMci-RmdO632mVtKRLFsRHRz/view?usp=drive_link",
       iconColor: "text-purple-600",
       buttonColor: "bg-purple-600 hover:bg-purple-700",
